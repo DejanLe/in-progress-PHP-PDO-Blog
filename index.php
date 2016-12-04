42fd60a10e38b0d2708de28cf3350f922cbf3971
dejanle <dejnik978@gmail.com>
2016-12-04 12:31:35 +0100

    add jumbotron

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php 
 include('header.php');
+include('jumbotron.php');
 ?>
 
-  <!--jumbotron-->
-  <div class="overlay">
-	  <div class="jumbotron">
-	  	<div class="container text-center">
-	  		<h1>Welcome to my blog</h1> 
-	  		<a href="insert.php"><button type="submit" class="btn btn-default">Insert new Post</button></a>  
-	  	</div>
-	  </div>
-  </div>
      <div class="container">
 			<div class="row"> 
 			<div class="col-lg-8 col-md-offset-1">
---
+++ jumbotron.php
@@ -0,0 +1,10 @@
+
+  <!--jumbotron-->
+  <div class="overlay">
+	  <div class="jumbotron">
+	  	<div class="container text-center">
+	  		<h1>Welcome to my blog</h1> 
+	  		<a href="insert.php"><button type="submit" class="btn btn-default">Insert new Post</button></a>  
+	  	</div>
+	  </div>
+  </div>
